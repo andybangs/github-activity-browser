4a4dda2d75e29ea1549c498cec3bb69d5c29cb31
Andy Bangs <andrew.yuta@gmail.com>
2016-02-21 12:31:40 -0800

    sort users alphabetically

--- src/Following.js
+++ src/Following.js
@@ -12,16 +12,30 @@ function Following({sources, props$}) {
     .mergeAll()
     .map(res => res.body)
     .startWith([])
-    .map(results =>
-      div([
-        ul({className: 'search-results'}, results.map(result =>
+    .map(results => {
+      const sortedResults = results
+        .map(user => {
+          return Object.assign({}, {
+            login: user.login.toLowerCase(),
+            avatar_url: user.avatar_url,
+            html_url: user.html_url,
+          });
+        })
+        .sort((a, b) => {
+          if (a.login > b.login) return 1;
+          if (a.login < b.login) return -1;
+          return 0;
+        });
+
+      return div([
+        ul({className: 'search-results'}, sortedResults.map(result =>
           li({className: 'search-result'}, [
             img({src: result.avatar_url, alt: result.login, height: "42", width: "42"}),
             a({href: result.html_url}, result.login),
           ])
         ))
       ])
-    );
+    });
 
   return {
     DOM: vtree$,
